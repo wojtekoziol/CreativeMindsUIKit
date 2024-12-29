28af2bb2230f1633397a0b8eaf4e340b57ed2de7
Wojciech <wojtek.koziol.pl@gmail.com>
2024-12-29 16:01:08 +0100

    Initial commit

---
+++ CreativeMindsUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreativeMindsUIKit
+//
+//  Created by Wojciech Kozioł on 29/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
